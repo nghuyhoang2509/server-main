49e182dddbfb09b6aa76f720daf734e5e21e635b
nghuyhoang2509 <nghuyhoang2509@gmail.com>
2022-08-14 20:44:51 +0700

    redis fix 3

--- src/config/redisDB.js
+++ src/config/redisDB.js
@@ -1,5 +1,5 @@
-import { createClient } from 'redis';
-const redisDB = createClient({
+const redis = require("redis")
+const redisDB = redis.createClient({
   url: 'redis://default:8f6oBHWPxShPpHNELeUT@containers-us-west-57.railway.app:6750'
 });
 
